81a58166b422099564188228b2b5fc4b001762fe
Robert Gleason <l2ob122@gmail.com>
2025-08-27 21:53:25 -0700

    add template functions for string manipulation and date formatting; create functions.tmpl for demonstration Showed how functions work in templates

--- Go/main.go
+++ Go/main.go
@@ -6,8 +6,20 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"strings"
+	"time"
 )
 
+// uppercase the string
+func toUpper(str string) string {
+	return strings.ToUpper(str)
+}
+
+// format the date
+func formatDate(t time.Time) string {
+	return t.Format("September 5, 1986")
+
+}
 func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
@@ -41,6 +53,34 @@ func main() {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 
+	})
+
+	http.HandleFunc("/functions", func(w http.ResponseWriter, r *http.Request) {
+
+		// our custom functions
+		funcMap := template.FuncMap{
+			"toUpper":    toUpper,
+			"formatDate": formatDate,
+		}
+
+		tmpl := template.Must(template.New("functions.tmpl").Funcs(funcMap).ParseFiles("templates/functions.tmpl"))
+
+		data := struct {
+			Name        string
+			CurrentDate time.Time
+			Number      int
+			Items       []string
+		}{
+			Name:        "Dr.Venkmin",
+			CurrentDate: time.Now(),
+			Number:      11,
+			Items:       []string{"Apple", "Banana", "Cherry"},
+		}
+
+		err := tmpl.Execute(w, data)
+		if err != nil {
+			http.Error(w, "error rendering template", http.StatusInternalServerError)
+		}
 	})
 
 	fmt.Println("Server starting on :8080")
